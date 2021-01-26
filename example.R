6a528f47fe280953889a1a663e489beeeb0d4fc1
Sean Anderson <sean@seananderson.ca>
2021-01-25 18:31:00 -0800

    Add to example; build in actual sampling

--- example.R
+++ example.R
@@ -11,9 +11,15 @@ sim <- SimSurvey::sim_abundance(ages = seq(1, 10), years = seq(1, 10)) %>%
     ays_covar = SimSurvey::sim_ays_covar(phi_age = 0.8, phi_year = 0.1),
     depth_par = SimSurvey::sim_parabola(mu = 200, sigma = 30)
   )
+xy_sim <- tibble::as_tibble(sim$grid_xy)
+df_sim <- tibble::as_tibble(sim$sp_N)
+df_sim <- left_join(df_sim, xy)
+df_sim_sum <- group_by(df_sim, year, x, y, depth, cell) %>%
+  summarise(N = sum(N), .groups = "drop")
 
-xy <- tibble::as_tibble(sim$grid_xy)
-df <- tibble::as_tibble(sim$sp_N)
+survey <- SimSurvey::sim_survey(sim)
+xy <- tibble::as_tibble(survey$grid_xy)
+df <- tibble::as_tibble(survey$sp_N)
 df <- left_join(df, xy)
 df_sum <- group_by(df, year, x, y, depth, cell) %>%
   summarise(N = sum(N), .groups = "drop")
@@ -40,26 +46,27 @@ max(df_sum$N)
 min(df_sum$N)
 range(df_sum$x)
 range(df_sum$y)
+nrow(df_sum)
 
 dat <- df_sum
-dat <- dplyr::filter(df_sum, N > 10) %>%
+# dat <- dplyr::filter(df_sum, N > 10) %>%
   # dat <- mutate(df_sum, N = if_else(N > 10, N, 0)) %>% # For Tweedie?
-  mutate(N1000 = N / 1000)
+  # mutate(N1000 = N / 1000)
 
-ggplot(dat, aes(x, y, fill = log(N1000))) +
-  geom_tile() +
-  facet_wrap(~year) +
-  scale_fill_viridis_c()
+# ggplot(dat, aes(x, y, fill = log(N1000))) +
+#   geom_tile() +
+#   facet_wrap(~year) +
+#   scale_fill_viridis_c()
 
-range(dat$N1000)
+# range(dat$N1000)
 
 mesh <- sdmTMB::make_mesh(dat, xy_cols = c("x", "y"), cutoff = 20)
 mesh$spde$mesh$n
 plot(mesh)
 
-fit <- sdmTMB(N1000 ~ 0 + as.factor(year),
+fit <- sdmTMB(N ~ 0 + as.factor(year),
   data = dat,
-  family = Gamma(link = "log"), spde = mesh,
+  family = sdmTMB::nbinom2(link = "log"), spde = mesh,
   include_spatial = FALSE, ar1_fields = TRUE, time = "year",
   silent = FALSE
 )
@@ -74,7 +81,8 @@ grid_dat <- tidyr::expand_grid(x = sort(unique(dat$x)), y = sort(unique(dat$y)))
 grid_dat <- purrr::map_dfr(sort(unique(dat$year)), ~ bind_cols(grid_dat, year = .))
 pred <- predict(fit, newdata = grid_dat, return_tmb_object = TRUE)
 
-ggplot(dat, aes(x, y, fill = log(N1000))) +
+dplyr::filter(df_sim_sum, N > 0, x > -110, x < 110) %>%
+  ggplot(aes(x, y, fill = log(N))) +
   geom_tile() +
   facet_wrap(~year) +
   scale_fill_viridis_c()
@@ -82,6 +90,17 @@ ggplot(dat, aes(x, y, fill = log(N1000))) +
 ggplot(pred$data, aes(x, y, fill = est)) +
   geom_tile() +
   facet_wrap(~year) +
+  scale_fill_viridis_c()
+
+true <- dplyr::filter(df_sim_sum, N > 0, x > -110, x < 110) %>%
+  select(x, y, year, N) %>% mutate(type = "True")
+fitted <- pred$data %>%
+  select(x, y, year, N = est) %>% mutate(type = "Predicted") %>%
+  mutate(N = exp(N))
+both <- bind_rows(true, fitted)
+ggplot(both, aes(x, y, fill = log(N))) +
+  geom_tile() +
+  facet_grid(type~year) +
   scale_fill_viridis_c()
 
 # This just looks weird:
@@ -89,8 +108,9 @@ ggplot(pred$data, aes(x, y, fill = est)) +
   geom_tile() +
   facet_wrap(~year) +
   scale_fill_viridis_c() +
-  geom_point(aes(x, y, size = log(N1000)),
-    inherit.aes = FALSE, pch = 21, alpha = 0.5, data = dat
+  geom_point(aes(x, y, size = log(N)),
+    inherit.aes = FALSE, pch = 21, alpha = 0.5,
+    data = dplyr::filter(df_sim_sum, N > 0, x > -110, x < 110)
   ) +
   scale_size_area()
 
@@ -100,3 +120,5 @@ ggplot(index, aes(year, est)) +
   geom_line() +
   geom_ribbon(aes(ymin = lwr, ymax = upr), alpha = 0.4) +
   labs(x = "Year", y = "Index")
+
+SimSurvey::plot_trend(survey)
